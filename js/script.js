5ed360032358c0908c2f2da66c970eeb3aae747b
Davide Francavilla <davfrancavilla@gmail.com>
2020-09-09 22:50:16 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -3,13 +3,95 @@ $(document).ready(function(){
 getList();
 
 
+$("#addElement").on("click", function () {
+    var elementName = $("input").val();
+    $("input").val("");
+    addItem(elementName);
+})
 
+$("#element-name").keydown(function (event) {
+    if (event.which == 13 || event.keyCode == 13) {
+        var elementName = $("input").val();
+        $("input").val("");
+        addItem(elementName);
+    }
+});
 
+$("#element-name").click(function(){
+    $("#list input").remove();
+    $("#list span").show();
+})
 
-$("#list").on("click", "i",function(){
+$("#list").on("click", "i", function () {
     var id = $(this).parent().attr("data-id");
     deleteItem(id);
 })
+
+$("#list").on("click", "span", function () {
+    $("#list input").remove();
+    $("#list span").show();
+
+    var name = $(this).text();
+    $(this).hide();
+    var source = $("#editor-template").html();
+    var template = Handlebars.compile(source);
+    var obj = {
+        text : name
+    }
+    var html = template(obj);
+    $(this).parent().prepend(html);
+})
+
+
+
+$("#list").on("keydown", "input", function (e) {
+    if (event.which == 13 || event.keyCode == 13) {
+        var id = $(this).parent().attr("data-id");
+        var name = $(this).val();
+        editElement(id,name)
+    }
+});
+
+
+
+
+
+function editElement(id,name) {
+    
+    $.ajax({
+        url: "http://157.230.17.132:3012/todos/"+id,
+        method: "PATCH",
+        data: {
+            text: name
+        },
+        success: function () {
+            getList();
+        },
+        error: function (error) {
+            alert(error);
+        }
+    });
+}
+
+
+
+
+
+function addItem(name){
+    $.ajax({
+        url: "http://157.230.17.132:3012/todos",
+        method : "POST",
+        data : {
+            text : name
+        },
+        success: function () {
+            getList();
+        },
+        error: function (error) {
+            alert(error);
+        }
+    });
+}
 
 
 function deleteItem(id){
